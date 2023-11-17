0a18f9e592560c00cbe0fc7dec9a9e9cd42e5687
Lola Erkin <lola.erkin@fortispay.com>
2023-11-17 10:23:27 -0600

    removed var

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  assume_role_with_web_identity {
-    role_arn = var.aws_role_arn
-    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
+  #assume_role_with_web_identity {
+  #  role_arn = var.aws_role_arn
+   # web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   
   }
 }
